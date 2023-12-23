159b8bef8d47470a178b0ed2b4bc8e2a2b48e2c3
dmiyakoshi <d1m354dai@gmail.com>
2023-12-23 18:33:56 +0900

    UserController 作成

--- routes/auth.php
+++ routes/auth.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
 use Laravel\Fortify\Features;
