098e7eb63398cbfe4755a1c5c20c27ceae242945
dmiyakoshi <d1m354dai@gmail.com>
2024-02-02 18:17:48 +0900

    スマホのみのアコーディオンメニュー作成 テストまだ

--- resources/views/plans/index.blade.php
+++ resources/views/plans/index.blade.php
@@ -21,9 +21,9 @@
     </div>
     <div class="container mx-auto w-full my-8 px-4 py-4 bg-white grid grid-cols-6">
         <div class="col-span-3  md:col-span-2">
-            <h3 class="mb-3 text-gray-400 text-xl text-center md:text-base accordionH3">検索条件</h3>
+            <h3 id="sp_accordionButton" class="mb-3 text-gray-400 text-xl text-center md:text-base">検索条件</h3>
             {{-- スマホの場合はここが丸ごとアコーディオンメニューになる --}}
-            <div>
+            <div id="sp_accordion">
                 <li class="mb-2 text-center">
                     <a href="/{{ empty($sort) ? '' : '?' . http_build_query($sort) }}"
                         class="hover:text-blue-500 {{ strpos(url()->full(), 'prefecture') ?: 'text-green-500 font-bold' }}">全て</a>
@@ -110,69 +110,6 @@
     </div>
     </div>
 
-    <script>
-        // メニューを開く関数
-        const slideDown = (el) => {
-            el.style.height = 'auto'; //いったんautoに
-            let h = el.offsetHeight; //autoにした要素から高さを取得
-            el.style.height = h + 'px';
-            el.animate([ //高さ0から取得した高さまでのアニメーション
-                {
-                    height: 0
-                },
-                {
-                    height: h + 'px'
-                }
-            ], {
-                duration: 300, //アニメーションの時間（ms）
-            });
-        };
-
-        // メニューを閉じる関数
-        const slideUp = (el) => {
-            el.style.height = 0;
-        };
-
-        let activeIndex = null; //開いているアコーディオンのindex
-
-        //アコーディオンコンテナ全てで実行
-        const accordions = document.querySelectorAll('ul.include-accordion');
-        accordions.forEach((accordion) => {
-
-            //アコーディオンボタン全てで実行
-            const accordionBtns = accordion.querySelectorAll('.accordionBtn');
-
-            accordionBtns.forEach((accordionBtn, index) => {
-                accordionBtn.addEventListener('click', (e) => {
-                    activeIndex = index; //クリックされたボタンを把握
-                    e.target.parentNode.classList.toggle('active'); //ボタンの親要素（=ul>li)にクラスを付与／削除
-                    const content = accordionBtn.nextElementSibling; //ボタンの次の要素（=ul>ul）
-                    if (e.target.parentNode.classList.contains('active')) {
-                        slideDown(content); //クラス名がactive（＝閉じていた）なら上記で定義した開く関数を実行
-                    } else {
-                        slideUp(content); //クラス名にactiveがない（＝開いていた）なら上記で定義した閉じる関数を実行
-                    }
-                    accordionBtns.forEach((accordionBtn, index) => {
-                        if (activeIndex !== index) {
-                            accordionBtn.parentNode.classList.remove('active');
-                            const openedContent = accordionBtn.nextElementSibling;
-                            slideUp(openedContent); //現在開いている他のメニューを閉じる
-                        }
-                    });
-                    //スクロール制御のために上位階層ulのクラス名を変える
-                    let container = accordion.closest(
-                        '.scroll-control'); //sroll-controlnのクラス名である親要素を取得
-                    if (accordionBtn.parentNode.classList.contains('active') == false &&
-                        container !== null) {
-                        container.classList.remove('active')
-                    } else if (container !== null) {
-                        container.classList.add('active')
-                    }
-                });
-            });
-        })
-    </script>
-
     <style>
         .accordionButton {
             text-align: left
@@ -221,7 +158,6 @@
             padding: 10px 0 10px 10px;
         }
 
-
         button {
             position: relative;
             border: none;
@@ -249,6 +185,18 @@
 
         ul.active {
             overflow-y: auto;
+        }
+
+        .sp_accrodion {
+            opacity: 0;
+            height: 0;
+            overflow: hidden;
+            transition-duration: .3s;
+        }
+
+        .sp_accrodion {
+            opacity: 1;
+            height: auto;
         }
     </style>
 </x-app-layout>
